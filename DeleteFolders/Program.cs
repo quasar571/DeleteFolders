45613d56f2dde2bae5ac34fcb96f206e1654fd4e
quasar571 <snigur97@yandex.ua>
2016-03-24 00:32:37 +0200

    Удаляет все bin и obj папки

---
+++ DeleteFolders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace DeleteFolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+            else
+            {
+                path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            }
+            DeleteDirectoryRecursive(path);
+        }
+
+        static void DeleteDirectoryRecursive(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (var dir in di.GetDirectories())
+            {
+                if (dir.Name == "bin" || dir.Name == "obj")
+                {
+                    dir.Delete(true);
+                }
+                else
+                {
+                    DeleteDirectoryRecursive(dir.FullName);
+                }
+            }
+        }
+    }
+}
